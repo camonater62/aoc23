2f6682a43535e02c46a95ce77b6c3bc29778b767
Cameron West <camonater62@gmail.com>
2023-12-05 21:34:49 -0800

    day6

---
+++ day6/main.cpp
@@ -0,0 +1,94 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(string str, string delimiter) {
+    vector<string> result;
+    int pos = 0;
+    string token;
+    while ((pos = str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        result.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+    result.push_back(str);
+    return result;
+}
+
+int main() {
+    ifstream input("input.txt");
+
+    string fullTime;
+    string fullDistance;
+
+    getline(input, fullTime);
+    getline(input, fullDistance);
+
+    fullTime = split(fullTime, ":")[1];
+    fullDistance = split(fullDistance, ":")[1];
+
+    vector<int> times;
+    vector<int> distances;
+
+    for (string s : split(fullTime, " ")) {
+        if (s.size() > 0) {
+            times.push_back(stoi(s));
+        }
+    }
+
+    for (string s : split(fullDistance, " ")) {
+        if (s.size() > 0) {
+            distances.push_back(stoi(s));
+        }
+    }
+
+    int total = 1;
+
+    for (int i = 0; i < times.size(); i++) {
+        int count = 0;
+        for (int j = 0; j <= times[i]; j++) {
+            int delay = j;
+            int speed = j;
+            int travelled = (times[i] - j) * speed;
+            // cout << travelled << endl;
+            if (travelled > distances[i]) {
+                count++;
+            }
+        }
+        total *= count;
+    }
+
+    cout << total << endl;
+
+    for (int i = fullTime.size() - 1; i >= 0; i--) {
+        if (fullTime[i] == ' ') {
+            fullTime.erase(fullTime.begin() + i);
+        }
+    }
+
+    for (int i = fullDistance.size() - 1; i >= 0; i--) {
+        if (fullDistance[i] == ' ') {
+            fullDistance.erase(fullDistance.begin() + i);
+        }
+    }
+
+    long bigTime = stol(fullTime);
+    long bigDistance = stol(fullDistance);
+
+    long count = 0;
+    for (long j = 0; j <= bigTime; j++) {
+        long delay = j;
+        long speed = j;
+        long travelled = (bigTime - j) * speed;
+        // cout << travelled << endl;
+        if (travelled > bigDistance) {
+            count++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
