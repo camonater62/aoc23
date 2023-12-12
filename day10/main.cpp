1fc0bee427c8664feebfe5e99f6fa7c1ae3d5076
Cameron West <camonater62@gmail.com>
2023-12-11 20:41:16 -0800

    Fix day10 for linux

--- day10/main.cpp
+++ day10/main.cpp
@@ -1,33 +1,26 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include "../common.hpp"
-#include <set>
+
+#include <climits>
+#include <fstream>
+#include <iostream>
 #include <queue>
+#include <set>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-enum class direction
-{
-    north,
-    south,
-    east,
-    west,
-    start
-};
+enum class direction { north, south, east, west, start };
 
-struct cell
-{
+struct cell {
     int x;
     int y;
     direction dir;
     int distance;
 };
 
-int main()
-{
+int main() {
     ifstream input("input.txt");
     string line;
 
@@ -37,15 +30,12 @@ int main()
 
     int x, y = 0;
 
-    while (getline(input, line))
-    {
+    while (getline(input, line)) {
         vector<char> row;
-        for (int x = 0; x < line.size(); x++)
-        {
+        for (int x = 0; x < line.size(); x++) {
             row.push_back(line[x]);
 
-            if (line[x] == 'S')
-            {
+            if (line[x] == 'S') {
                 startx = x;
                 starty = y;
             }
@@ -68,149 +58,94 @@ int main()
     set<pair<int, int>> walls;
     queue<cell> q;
 
-    q.push({startx, starty, direction::start, 0});
+    q.push({ startx, starty, direction::start, 0 });
 
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         auto [x, y, dir, distance] = q.front();
         q.pop();
 
-        if (visited.find({x, y}) != visited.end())
-        {
+        if (visited.find({ x, y }) != visited.end()) {
             continue;
         }
 
-        visited.insert({x, y});
+        visited.insert({ x, y });
 
         bool found = true;
-        switch (dir)
-        {
+        switch (dir) {
         case direction::north:
-            if (map[y - 1][x] == '|')
-            {
-                q.push({x, y - 1, direction::north, distance + 1});
-            }
-            else if (map[y - 1][x] == 'F')
-            {
-                q.push({x, y - 1, direction::east, distance + 1});
-            }
-            else if (map[y - 1][x] == '7')
-            {
-                q.push({x, y - 1, direction::west, distance + 1});
-            }
-            else if (map[y - 1][x] == 'S')
-            {
+            if (map[y - 1][x] == '|') {
+                q.push({ x, y - 1, direction::north, distance + 1 });
+            } else if (map[y - 1][x] == 'F') {
+                q.push({ x, y - 1, direction::east, distance + 1 });
+            } else if (map[y - 1][x] == '7') {
+                q.push({ x, y - 1, direction::west, distance + 1 });
+            } else if (map[y - 1][x] == 'S') {
                 cout << "P1: " << (distance + 1) / 2 << endl;
-            }
-            else
-            {
+            } else {
                 found = false;
             }
             break;
         case direction::south:
-            if (map[y + 1][x] == '|')
-            {
-                q.push({x, y + 1, direction::south, distance + 1});
-            }
-            else if (map[y + 1][x] == 'L')
-            {
-                q.push({x, y + 1, direction::east, distance + 1});
-            }
-            else if (map[y + 1][x] == 'J')
-            {
-                q.push({x, y + 1, direction::west, distance + 1});
-            }
-            else if (map[y + 1][x] == 'S')
-            {
+            if (map[y + 1][x] == '|') {
+                q.push({ x, y + 1, direction::south, distance + 1 });
+            } else if (map[y + 1][x] == 'L') {
+                q.push({ x, y + 1, direction::east, distance + 1 });
+            } else if (map[y + 1][x] == 'J') {
+                q.push({ x, y + 1, direction::west, distance + 1 });
+            } else if (map[y + 1][x] == 'S') {
                 cout << "P1: " << (distance + 1) / 2 << endl;
-            }
-            else
-            {
+            } else {
                 found = false;
             }
             break;
         case direction::east:
-            if (map[y][x + 1] == '-')
-            {
-                q.push({x + 1, y, direction::east, distance + 1});
-            }
-            else if (map[y][x + 1] == '7')
-            {
-                q.push({x + 1, y, direction::south, distance + 1});
-            }
-            else if (map[y][x + 1] == 'J')
-            {
-                q.push({x + 1, y, direction::north, distance + 1});
-            }
-            else if (map[y][x + 1] == 'S')
-            {
+            if (map[y][x + 1] == '-') {
+                q.push({ x + 1, y, direction::east, distance + 1 });
+            } else if (map[y][x + 1] == '7') {
+                q.push({ x + 1, y, direction::south, distance + 1 });
+            } else if (map[y][x + 1] == 'J') {
+                q.push({ x + 1, y, direction::north, distance + 1 });
+            } else if (map[y][x + 1] == 'S') {
                 cout << "P1: " << (distance + 1) / 2 << endl;
-            }
-            else
-            {
+            } else {
                 found = false;
             }
             break;
         case direction::west:
-            if (map[y][x - 1] == '-')
-            {
-                q.push({x - 1, y, direction::west, distance + 1});
-            }
-            else if (map[y][x - 1] == 'F')
-            {
-                q.push({x - 1, y, direction::south, distance + 1});
-            }
-            else if (map[y][x - 1] == 'L')
-            {
-                q.push({x - 1, y, direction::north, distance + 1});
-            }
-            else if (map[y][x - 1] == 'S')
-            {
+            if (map[y][x - 1] == '-') {
+                q.push({ x - 1, y, direction::west, distance + 1 });
+            } else if (map[y][x - 1] == 'F') {
+                q.push({ x - 1, y, direction::south, distance + 1 });
+            } else if (map[y][x - 1] == 'L') {
+                q.push({ x - 1, y, direction::north, distance + 1 });
+            } else if (map[y][x - 1] == 'S') {
                 cout << "P1: " << (distance + 1) / 2 << endl;
-            }
-            else
-            {
+            } else {
                 found = false;
             }
             break;
         case direction::start:
-            if (map[y][x + 1] == '-')
-            {
-                q.push({x + 1, y, direction::east, distance + 1});
-            }
-            else if (map[y][x - 1] == '-')
-            {
-                q.push({x - 1, y, direction::west, distance + 1});
-            }
-            else if (map[y - 1][x] == '|')
-            {
-                q.push({x, y - 1, direction::north, distance + 1});
-            }
-            else if (map[y + 1][x] == '|')
-            {
-                q.push({x, y + 1, direction::south, distance + 1});
-            }
-            else if (map[y][x + 1] == '7')
-            {
-                q.push({x + 1, y, direction::south, distance + 1});
-            }
-            else if (map[y][x + 1] == 'J')
-            {
-                q.push({x + 1, y, direction::north, distance + 1});
-            }
-            else if (map[y][x - 1] == 'F')
-            {
-                q.push({x - 1, y, direction::south, distance + 1});
-            }
-            else if (map[y][x - 1] == 'L')
-            {
-                q.push({x - 1, y, direction::north, distance + 1});
+            if (map[y][x + 1] == '-') {
+                q.push({ x + 1, y, direction::east, distance + 1 });
+            } else if (map[y][x - 1] == '-') {
+                q.push({ x - 1, y, direction::west, distance + 1 });
+            } else if (map[y - 1][x] == '|') {
+                q.push({ x, y - 1, direction::north, distance + 1 });
+            } else if (map[y + 1][x] == '|') {
+                q.push({ x, y + 1, direction::south, distance + 1 });
+            } else if (map[y][x + 1] == '7') {
+                q.push({ x + 1, y, direction::south, distance + 1 });
+            } else if (map[y][x + 1] == 'J') {
+                q.push({ x + 1, y, direction::north, distance + 1 });
+            } else if (map[y][x - 1] == 'F') {
+                q.push({ x - 1, y, direction::south, distance + 1 });
+            } else if (map[y][x - 1] == 'L') {
+                q.push({ x - 1, y, direction::north, distance + 1 });
             }
             break;
         }
-        if (found)
-        {
-            walls.insert({x, y});
+        if (found) {
+            walls.insert({ x, y });
         }
     }
 
@@ -219,48 +154,34 @@ int main()
     int left = INT_MAX;
     int right = INT_MIN;
 
-    for (auto [x, y] : walls)
-    {
-        if (x < left)
-        {
+    for (auto [x, y] : walls) {
+        if (x < left) {
             left = x;
         }
-        if (x > right)
-        {
+        if (x > right) {
             right = x;
         }
-        if (y < top)
-        {
+        if (y < top) {
             top = y;
         }
-        if (y > bottom)
-        {
+        if (y > bottom) {
             bottom = y;
         }
     }
 
     int count = 0;
-    for (int y = top; y <= bottom; y++)
-    {
+    for (int y = top; y <= bottom; y++) {
         bool inside = false;
-        for (int x = left; x <= right; x++)
-        {
-            if (walls.find({x, y}) != walls.end())
-            {
-                switch (map[y][x])
-                {
+        for (int x = left; x <= right; x++) {
+            if (walls.find({ x, y }) != walls.end()) {
+                switch (map[y][x]) {
                 case '|':
                 case 'J':
                 case 'L':
-                case 'S':
-                    inside = !inside;
-                    break;
-                default:
-                    break;
+                case 'S': inside = !inside; break;
+                default: break;
                 }
-            }
-            else if (inside)
-            {
+            } else if (inside) {
                 count++;
             }
         }
