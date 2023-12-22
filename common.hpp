6fcb6d858795da9eb2fdcdfd7c0882a1bfe01d64
Cameron West <camonater62@gmail.com>
2023-12-21 22:41:49 -0800

    Day 22

--- common.hpp
+++ common.hpp
@@ -2,12 +2,15 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
 
-std::vector<std::string> split(std::string str, std::string delimiter) {
+std::vector<std::string> split(std::string str, std::string delimiter)
+{
     std::vector<std::string> result;
     int pos = 0;
     std::string token;
-    while ((pos = str.find(delimiter)) != std::string::npos) {
+    while ((pos = str.find(delimiter)) != std::string::npos)
+    {
         token = str.substr(0, pos);
         result.push_back(token);
         str.erase(0, pos + delimiter.length());
@@ -16,7 +19,9 @@ std::vector<std::string> split(std::string str, std::string delimiter) {
     return result;
 }
 
-template <typename Iterable> class enumerate_object {
+template <typename Iterable>
+class enumerate_object
+{
 private:
     Iterable _iter;
     std::size_t _size;
@@ -25,82 +30,108 @@ private:
 
 public:
     enumerate_object(Iterable iter)
-        : _iter(iter)
-        , _size(0)
-        , _begin(std::begin(iter))
-        , _end(std::end(iter)) {
+        : _iter(iter), _size(0), _begin(std::begin(iter)), _end(std::end(iter))
+    {
     }
 
-    const enumerate_object &begin() const {
+    const enumerate_object &begin() const
+    {
         return *this;
     }
-    const enumerate_object &end() const {
+    const enumerate_object &end() const
+    {
         return *this;
     }
 
-    bool operator!=(const enumerate_object &) const {
+    bool operator!=(const enumerate_object &) const
+    {
         return _begin != _end;
     }
 
-    void operator++() {
+    void operator++()
+    {
         ++_begin;
         ++_size;
     }
 
-    auto operator*() const -> std::pair<std::size_t, decltype(*_begin)> {
-        return { _size, *_begin };
+    auto operator*() const -> std::pair<std::size_t, decltype(*_begin)>
+    {
+        return {_size, *_begin};
     }
 };
 
-template <typename Iterable> auto enumerate(Iterable &&iter) -> enumerate_object<Iterable> {
-    return { std::forward<Iterable>(iter) };
+template <typename Iterable>
+auto enumerate(Iterable &&iter) -> enumerate_object<Iterable>
+{
+    return {std::forward<Iterable>(iter)};
 }
 
-template <class T> inline void hash_combine(std::size_t &seed, const T &v) {
+template <class T>
+inline void hash_combine(std::size_t &seed, const T &v)
+{
     std::hash<T> hasher;
     seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
-namespace std {
-template <typename T1, typename T2> struct hash<std::pair<T1, T2>> {
-    size_t operator()(const std::pair<T1, T2> &p) const {
-        std::size_t seed1(0);
-        ::hash_combine(seed1, p.first);
-        ::hash_combine(seed1, p.second);
+namespace std
+{
+    template <typename T1, typename T2>
+    struct hash<std::pair<T1, T2>>
+    {
+        size_t operator()(const std::pair<T1, T2> &p) const
+        {
+            std::size_t seed1(0);
+            ::hash_combine(seed1, p.first);
+            ::hash_combine(seed1, p.second);
 
-        return seed1;
-    }
-};
+            return seed1;
+        }
+    };
 }
 
-template <> struct std::hash<std::vector<int>> {
-    std::size_t operator()(const std::vector<int> &vec) const {
+template <>
+struct std::hash<std::vector<int>>
+{
+    std::size_t operator()(const std::vector<int> &vec) const
+    {
         std::size_t seed = vec.size();
-        for (auto &i : vec) {
+        for (auto &i : vec)
+        {
             seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
         }
         return seed;
     }
 };
 
-template <typename T> std::vector<T> subvec(const std::vector<T> arr) {
-    if (arr.size() <= 1) {
+template <typename T>
+std::vector<T> subvec(const std::vector<T> arr)
+{
+    if (arr.size() <= 1)
+    {
         return {};
-    } else {
+    }
+    else
+    {
         return std::vector<T>(arr.begin() + 1, arr.end());
     }
 }
 
-template <typename T> T sum(const std::vector<T> &arr) {
+template <typename T>
+T sum(const std::vector<T> &arr)
+{
     T total = 0;
-    for (const T &i : arr) {
+    for (const T &i : arr)
+    {
         total += i;
     }
     return total;
 }
 
-template <typename T> T gcd(T a, T b) {
-    while (b != 0) {
+template <typename T>
+T gcd(T a, T b)
+{
+    while (b != 0)
+    {
         T temp = b;
         b = a % b;
         a = temp;
@@ -108,16 +139,37 @@ template <typename T> T gcd(T a, T b) {
     return a;
 }
 
-template <typename T> T lcm(T a, T b) {
+template <typename T>
+T lcm(T a, T b)
+{
     return (a * b) / (gcd(a, b));
 }
 
-template <typename T> T findLCM(const std::vector<T> &arr) {
+template <typename T>
+T findLCM(const std::vector<T> &arr)
+{
     T ans = arr[0];
 
-    for (size_t i = 1; i < arr.size(); i++) {
+    for (size_t i = 1; i < arr.size(); i++)
+    {
         ans = lcm(ans, arr[i]);
     }
 
     return ans;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i < v.size() - 1)
+        {
+            os << ",";
+        }
+    }
+    os << "]";
+    return os;
 }
---
+++ day22/main.cpp
@@ -0,0 +1,152 @@
+// leijurv
+
+#include "../common.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+struct Cube
+{
+    union
+    {
+        struct
+        {
+            int x, y, z;
+        };
+        int coords[3];
+    };
+
+    bool operator==(const Cube &other) const
+    {
+        return x == other.x && y == other.y && z == other.z;
+    }
+};
+
+struct Brick
+{
+    vector<Cube> cubes;
+};
+
+pair<vector<Brick>, int> dropBricks(vector<Brick> bricks)
+{
+    sort(bricks.begin(), bricks.end(), [](const Brick a, const Brick b)
+         {
+        int mina = INT_MAX;
+        for (Cube cube : a.cubes)
+        {
+            mina = min(mina, cube.z);
+        }
+        int minb = INT_MAX;
+        for (Cube cube : b.cubes)
+        {
+            minb = min(minb, cube.z);
+        }
+
+        return mina < minb; });
+
+    vector<Cube> settled;
+    vector<Brick> finalBricks;
+    int numFell = 0;
+
+    for (Brick brick : bricks)
+    {
+        bool moved = false;
+        while (true)
+        {
+            vector<Cube> next;
+            for (Cube cube : brick.cubes)
+            {
+                next.push_back({cube.x, cube.y, cube.z - 1});
+            }
+
+            if (all_of(next.begin(), next.end(), [&settled](const Cube &cube)
+                       { return cube.z != 0 && find(settled.begin(), settled.end(), cube) == settled.end(); }))
+            {
+                moved = true;
+                brick.cubes = next;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        for (Cube cube : brick.cubes)
+        {
+            settled.push_back(cube);
+        }
+        finalBricks.push_back(brick);
+        if (moved)
+        {
+            numFell++;
+        }
+    }
+
+    return {finalBricks, numFell};
+}
+
+int main()
+{
+    ifstream input("example.txt");
+    string line;
+
+    vector<Brick> bricks;
+
+    while (getline(input, line))
+    {
+        vector<string> parts = split(line, "~");
+        vector<string> startVec = split(parts[0], ",");
+        vector<string> endVec = split(parts[1], ",");
+        Cube start = {stoi(startVec[0]), stoi(startVec[1]), stoi(startVec[2])};
+        Cube end = {stoi(endVec[0]), stoi(endVec[1]), stoi(endVec[2])};
+        vector<Cube> cubes;
+
+        cubes.push_back(start);
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (start.coords[i] != end.coords[i])
+            {
+                for (int j = start.coords[i] + 1; j < end.coords[i]; j++)
+                {
+                    Cube cube = start;
+                    cube.coords[i] = j;
+                    cubes.push_back(cube);
+                }
+            }
+        }
+
+        cubes.push_back(end);
+
+        bricks.push_back({cubes});
+    }
+
+    vector<Brick> dropped = dropBricks(bricks).first;
+
+    int p1 = 0;
+    int p2 = 0;
+
+    for (int i = 0; i < dropped.size(); i++)
+    {
+        vector<Brick> without = dropped;
+        without.erase(without.begin() + i);
+        int num_fell = dropBricks(without).second;
+        if (num_fell == 0)
+        {
+            p1++;
+        }
+        p2 += num_fell;
+    }
+
+    cout << "P1: " << p1 << endl;
+    cout << "P2: " << p2 << endl;
+
+    return 0;
+}
